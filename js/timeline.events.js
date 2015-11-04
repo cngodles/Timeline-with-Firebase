0fff94418394d02818ffb97b05a13e9460080269
Clinton N,. Godlesky <cngodles@gmail.com>
2015-11-04 15:07:44 -0500

    Base Install

---
+++ js/timeline.events.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    //alert('hi');
+    
+    timeline.start = new Date(2010, 5, 14, 7, 34, 0, 0);
+    timeline.run();
+    timeline.init();
+    
+    $("#time").append('<div style="width:205px; height:30px; background-color:red; position:absolute; top:170px; left:205px;"></div>');
+})
+.on("click", ".action_addevent", function(e){
+    e.preventDefault();
+    timeline.firebase.push({'name':'Sample Event','length':7,'startdate':'2010-6-25'});
+})
+;
---
+++ js/timeline.main.js
@@ -0,0 +1,76 @@
+var timeline = {
+    start: '',
+    steps: 0,
+    year: 0,
+    month: '',
+    width: 41,
+    firebase: false,
+    events: [],
+    monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    init: function () {
+        var thisobj = this;
+        thisobj.firebase = new Firebase('https://ralena.firebaseio.com/timeline/events');
+        thisobj.firebase.on('value', function (snapshot) {
+            thisobj.events = snapshot.val();
+            thisobj.addEvents();
+        });
+    },
+    addEvents: function () {
+        $("#time .event").remove();
+        for (i = 0; i < this.timeline.events.length; i++) {
+            $("#time").append('<div class="event" style="width:205px; height:30px; background-color:red; position:absolute; top:170px; left:205px;"></div>');
+        }
+    },
+    run: function () {
+        var start = new Date("06/14/2010");
+        var end = new Date();
+
+
+
+        var push = [];
+        push.push('<div style="display:none">');
+        push.push('<div style="display:none">');
+
+        while (start < end) {
+            //The month is the current month + 1
+            var month = start.getMonth() + 1;
+            var year = start.getFullYear();
+
+
+            if (year != this.year) {
+                //Push in the HTML for the year.
+                push.push('</div></div><div class="year"><h2>' + start.getFullYear() + '</h2>');
+                push.push('<div style="display:none">');
+                this.year = year;
+            }
+
+            if (month != this.month) {
+                //Push in the HTML for the month.
+                push.push('</div><div class="month"><h3>' + this.monthNames[month - 1] + '</h3>');
+                this.month = month;
+            }
+
+            //Push in the HTML for the day.
+            push.push('<div class="col">' + month + '/' + start.getDate() + '</div>');
+
+            var newDate = start.setDate(start.getDate() + 1);
+            start = new Date(newDate);
+            this.steps++;
+
+        }
+        push.push('</div>');
+        push.push('</div>');
+        $("#time").html(push.join(''));
+        $("#time").css('width', this.steps * this.width);
+
+        $(".month").each(function () {
+            var totitems = $(this).find(".col").length;
+            $(this).width(totitems * this.width);
+        });
+        $(".year").each(function () {
+            var totitems = $(this).find(".col").length;
+            $(this).width(totitems * this.width);
+        });
+    }
+
+}
